f3355bcead807ecc70a9b2e63240b4dfc1350a19
James Priest <james-priest@outlook.com>
2017-10-11 01:12:31 -0700

    Add prototype getter code

---
+++ f-prototype-getters.js
@@ -0,0 +1,54 @@
+const test = require( 'tape' );
+/**
+ * JavaScript prototype getters with private data properties
+ * 
+ */
+
+function Vehicle( theYear, theMake, theModel ) {
+    var year = theYear; //private
+    var make = theMake; // private
+    var model = theModel; // private
+    this.getYear = function() { return year; };
+    this.getMake = function() { return make; };
+    this.getModel = function() { return model; };
+}
+Vehicle.prototype.getInfo = function() {
+    return 'Vehicle: ' + this.getYear() + ' ' + this.getMake() + ' ' + this.getModel();
+};
+
+test( "Instance Test Using Prototype and getters", function( t ) {
+    t.plan( 6 );
+    var actual, expected;
+
+    var car1 = new Vehicle( 2000, 'Ford', 'Fusion' );
+    t.ok( car1, "car1 exists" );
+
+    var car2 = new Vehicle( 2010, 'BMW', 'Z4' );
+    t.ok( car2, "car2 exists" );
+
+    actual = car1.getInfo();
+    expected = "Vehicle: 2000 Ford Fusion";    
+    t.isEqual( actual, expected, "car1.getInfo() matches expected" );
+
+    actual = car2.getInfo();
+    expected = "Vehicle: 2010 BMW Z4";    
+    t.isEqual( actual, expected, "car2.getInfo() matches expected" );
+
+    Vehicle.prototype.getInfo = function() {
+        return 'Car Year: ' + this.getYear()
+            + ' Make: ' + this.getMake()
+            + ' Model: ' + this.getModel();
+    };
+    console.log( "--- Update prototype getInfo method ---" );
+
+    actual = car1.getInfo();
+    expected = "Car Year: 2000 Make: Ford Model: Fusion";    
+    t.isEqual( actual, expected, "car1.getInfo() matches expected" );
+
+    actual = car2.getInfo();
+    expected = "Car Year: 2010 Make: BMW Model: Z4";    
+    t.isEqual( actual, expected, "car2.getInfo() matches expected" );
+} );
+
+// Run code:
+// node c-object-encapsulation.js
